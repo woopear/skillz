a6d946d579e37256b98e4005bea62054fbd797b5
john_dev <johnhaimez314@gmail.com>
2022-04-26 17:11:29 +0200

    competence state terminé

--- lib/src/models_shared/competence/state/competence_state.dart
+++ lib/src/models_shared/competence/state/competence_state.dart
@@ -1,28 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:skillz/src/models_shared/competence/schema/competence_schema.dart';
+import 'package:skillz/src/utils/fire/firestorepath.dart';
 import 'package:woo_firestore_crud/woo_firestore_crud.dart';
 
 class CompetenceState extends ChangeNotifier {
   final _firestore = WooFirestore.instance;
 
+  /// get all
   Future<List<CompetenceSchema>> getAllCompetence() async {
     final result = await _firestore.getCol(
-      path: path,
+      path: FirestorePath.competences(),
       builder: (data, documentId) => CompetenceSchema.formMap(data, documentId),
     );
     return result;
   }
 
-  Future<DocumentReference<Map<String, dynamic>?>?> addCompetence(CompetenceSchema newCompetence) async {
-    final result = await _firestore.add(
-      path: path,
-      data: newCompetence.toMap(),
+  /// get one
+  Future<DocumentSnapshot<Map<String, dynamic>>> getOneCompetence(
+    String idCompetence,
+  ) async {
+    final result = await _firestore.getDoc(
+      path: FirestorePath.competence(idCompetence),
     );
     return result;
   }
 
+  /// add
+  Future<DocumentReference<Map<String, dynamic>?>?> addCompetence(
+    CompetenceSchema newCompetence,
+  ) async {
+    final result = await _firestore.add(
+      path: FirestorePath.competences(),
+      data: newCompetence.toMap(),
+      returnData: true,
+    );
+    return result;
+  }
+
+  /// update
   Future<void> updateCompetence(
-      String idCompetence, CompetenceSchema newCompetence) async {}
-  Future<void> deleteCompetence(String idCompetence) async {}
+    String idCompetence,
+    CompetenceSchema newCompetence,
+  ) async {
+    await _firestore.update(
+      path: FirestorePath.competence(idCompetence),
+      data: newCompetence.toMap(),
+    );
+  }
+
+  /// delete
+  Future<void> deleteCompetence(String idCompetence) async {
+    await _firestore.delete(path: FirestorePath.competence(idCompetence));
+  }
 }
