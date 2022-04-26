4968a23dfb5f2fa339988322f89a0abc61dbee73
david-caignaert <davidcaignaert@gmail.com>
2022-04-26 15:45:08 +0200

    ajout path

--- lib/src/utils/fire/firestorepath.dart
+++ lib/src/utils/fire/firestorepath.dart
@@ -3,9 +3,17 @@ class FirestorePath {
   static String settings() => 'settings';
   static String setting(String idSetting) => 'settings/$idSetting';
 
+  /// condition
+  static String conditions() => 'conditions';
+  static String condition(String idCondition) => 'conditions/$idCondition';
+
   /// articleCondition
   static String articlesCondition(String idCondition) =>
       'conditions/$idCondition/articles';
   static String articleCondition(String idCondition, String idArticle) =>
       'conditions/$idCondition/articles/$idArticle';
+
+  /// level
+  static String levels() => 'levels';
+  static String level(String idLevel) => 'levels/$idLevel';
 }
