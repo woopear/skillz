783d6e8a37a9d991b6b975ddbe17a503e9e121a3
john_dev <johnhaimez314@gmail.com>
2022-04-27 09:35:42 +0200

    debut walletCompetence

---
+++ lib/src/models/profil/models/wallet_competence/state/wallet_competence_profil_state.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:woo_firestore_crud/woo_firestore_crud.dart';
+
+class WalletCompetenceProfilState extends ChangeNotifier {
+  final _firestore = WooFirestore.instance;
+
+  /* stream du wallet competence
+  Stream<WalletCompetenceProfilSchema> streamWalletCompetence() {
+
+  }*/
+}
