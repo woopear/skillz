4472262f6da18cdecf5cfc8b73380dec5544b896
john_dev <johnhaimez314@gmail.com>
2022-04-27 10:14:43 +0200

    debut model state workstation

---
+++ lib/src/models/profil/models/workstation/state/workstation_profil_state.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:woo_firestore_crud/woo_firestore_crud.dart';
+
+class WorkstationProfilState extends ChangeNotifier {
+  final _firestore = WooFirestore.instance;
+}
