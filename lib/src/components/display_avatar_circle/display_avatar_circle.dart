0b29e5e8f2526a7fd691f65f69c97ad09fe6e4b9
john_dev <johnhaimez314@gmail.com>
2022-04-27 16:48:11 +0200

    component display avatar circle

---
+++ lib/src/components/display_avatar_circle/display_avatar_circle.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Widget displayAvatartCircle({
+  String? image,
+  double size = 150,
+  EdgeInsetsGeometry margin = const EdgeInsets.only(top: 10.0, bottom: 10.0),
+  required BuildContext context,
+}) => Container(
+      margin: margin,
+      decoration: BoxDecoration(
+        color: Theme.of(context).brightness == Brightness.dark
+            ? Colors.grey
+            : Colors.black12,
+        shape: BoxShape.circle,
+      ),
+      child: image != null || image != ''
+          ? CircleAvatar(
+              radius: size / 2,
+              backgroundImage: NetworkImage(
+                image!,
+              ),
+            )
+          : Icon(Icons.person, size: size),
+    );
