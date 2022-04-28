12b2502f0e38f570bb3528798619f7980f0426f9
david-caignaert <davidcaignaert@gmail.com>
2022-04-28 14:55:03 +0200

    ajout du component input_password

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skillz/src/components/btn_close/btn_close.dart';
 import 'package:skillz/src/components/btn_save/btn_save.dart';
 import 'package:skillz/src/components/input_basic/input_basic.dart';
+import 'package:skillz/src/components/input_password/input_password.dart';
 import 'package:skillz/src/components/label_input/label_input.dart';
 import 'package:skillz/src/models/setting/state/setting_provider.dart';
 import 'package:skillz/pages/error_page.dart';
@@ -71,6 +72,10 @@ class _HomePageState extends ConsumerState<HomePage> {
                           hintText: 'le texte',
                           maxLines: 6,
                         ),
+                        InputPassword(
+                          labelText: 'mot de passe',
+                          controller: TextEditingController(text: ''),
+                        )
                       ],
                     ),
                   ),
