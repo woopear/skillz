bcd52da9972986b80b8fbe81171de27c273a3a2b
john_dev <johnhaimez314@gmail.com>
2022-06-11 08:33:19 +0200

    delete lit solidjs

--- skillz/resources/js/app.js
+++ skillz/resources/js/app.js
@@ -4,26 +4,3 @@ import "unpoly/unpoly.css";
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
 Alpine.start();
-
-import { createSignal, onCleanup } from "solid-js";
-import { render } from "solid-js/web";
-
-const App = () => {
-  const [count, setCount] = createSignal(0),
-    timer = setInterval(() => setCount(count() + 1), 1000);
-
-  onCleanup(() => clearInterval(timer));
-
-  const style = `
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    font-size:128px;
-    font-weight:bold;
-    height:100vh;
-  `;
-  return <div style={style}>{count()}</div>;
-};
-
-render(() => <App />, document.getElementById("composant"));
